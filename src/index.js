63011dff9666c1aae725811376882bfedf763ed5
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-09-15 14:21:06 -0300

    adicionando um hello world de teste

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ const express = require('express');
 
 const app = express();
 
+
+app.get('/projects', (req, res) => {
+  return res.send('Hello world');
+});
+
 // expondo a porta que a aplicação vai rodar
 app.listen(3333);
