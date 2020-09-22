66d9329f7fa8c970163989b5909b1ee1682a405e
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-09-22 11:23:33 -0300

    feat: Salvando, atualizando, buscando e deletando o projeto localmente

--- src/index.js
+++ src/index.js
@@ -1,54 +1,77 @@
-const { response } = require('express');
-const express = require('express');
+const { response } = require("express");
+const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/projects', (request, response) => {
+//vai servir como o nosso banco de dados em memória
+const projects = [];
+
+app.get("/projects", (request, response) => {
   //pegando os query params
-  const {title, owner} = request.query;
+  const { title, owner } = request.query;
 
-  console.log(title);
-  console.log(owner);
+  // console.log(title);
+  // console.log(owner);
 
-  return response.json([
-    'projeto 1',
-    'projeto 2'
-  ]);
+  return response.json(projects);
 });
 
+app.put("/projects/:id", (request, response) => {
+  const { id } = request.params;
 
-app.put('/projects/:id', (request, response) => {
+  //buscando os dados do body
+  const { title, owner } = request.body;
 
-  const {id} = request.params;
+  //buscando a posição do projeto no array de projetos
+  const projectIndex = projects.findIndex((project) => project.id === id);
 
-  console.log(id)
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: "Project not found" });
+  }
 
-  return response.json([
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  //substituindo o projeto existente pelos novo projeto criado com os dados passados pelo body
+  projects[projectIndex] = project;
+
+  //sempre que atualizar, retornar APENAS o projeto atualizado, NUNCA a lista completa
+  return response.json(project);
 });
 
-app.post('/projects', (request, response) => {
+app.post("/projects", (request, response) => {
+  const { title, owner } = request.body;
 
-  const {title, owner} = request.body;
+  const project = { id: uuid(), title, owner };
 
-  console.log(title)
-  console.log(owner)
+  projects.push(project);
 
-  return response.json([
+  return response.json(project);
+});
 
-    'Projeto 1',
-    'Projeto 2',
-    'Projeto 3',
-  ])
+app.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
 
+  //buscando a posição do projeto no array de projetos
+  const projectIndex = projects.findIndex((project) => project.id === id);
+
+  if (projectIndex < 0) {
+    return response.status(404).json({ error: 'Project not found.' });
+  }
+
+  //removendo o projeto
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 // expondo a porta que a aplicação vai rodar
 app.listen(3333, () => {
-  console.log('Back-end started!')
+  console.log("Back-end started!");
 });
