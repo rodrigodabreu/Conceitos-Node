174f4ea7ddecce4225b77b9da95003aec7f0012b
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-09-22 09:43:14 -0300

    feature: Adicionando rotas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,51 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
+app.use(express.json())
 
 app.get('/projects', (request, response) => {
-  return response.json({ message: 'Hello Rodrigo' });
+  //pegando os query params
+  const {title, owner} = request.query;
+
+  console.log(title);
+  console.log(owner);
+
+  return response.json([
+    'projeto 1',
+    'projeto 2'
+  ]);
+});
+
+
+app.put('/projects/:id', (request, response) => {
+
+  const {id} = request.params;
+
+  console.log(id)
+
+  return response.json([
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+});
+
+app.post('/projects', (request, response) => {
+
+  const {title, owner} = request.body;
+
+  console.log(title)
+  console.log(owner)
+
+  return response.json([
+
+    'Projeto 1',
+    'Projeto 2',
+    'Projeto 3',
+  ])
+
 });
 
 // expondo a porta que a aplicação vai rodar
