61368b12ab2edb472c95292dfa34b5a8098733ef
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2020-09-22 14:22:50 -0300

    feat: Adicionando o middleware de validar id

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const express = require("express");
-const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -12,11 +12,25 @@ const projects = [];
 function logRequests(request, response, next) {
   const { method, url } = request;
   const logLabel = `[${method.toUpperCase()}] ${url}`;
-  console.log(logLabel);
-  return next();
+  console.time(logLabel);
+  next();
+  console.timeEnd(logLabel);
 }
 
+function validateProjectId(request, response, next) {
+  const { id } = request.params;
+
+  if(!isUuid(id)){
+    return response.status(400).json({error: 'Invalid project ID.'});
+  }
+  return next();
+
+}
+//Middleware que loga as requisições
 app.use(logRequests);
+
+//Middleware que valida o id
+app.use('/projects/:id', validateProjectId);
 
 app.get("/projects", (request, response) => {
   //pegando os query params
@@ -84,5 +98,5 @@ app.delete("/projects/:id", (request, response) => {
 
 // expondo a porta que a aplicação vai rodar
 app.listen(3333, () => {
-  console.log("Back-end started!");
+  console.log("🚀 Back-end started!");
 });
